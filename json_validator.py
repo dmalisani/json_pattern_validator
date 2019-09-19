584032444f47d379c0f0388fda4675a0265dc7e7
Daniel <daniel.malisani@intive.com>
2019-09-19 12:26:10 -0300

    agregados validadores

--- json_validator.py
+++ json_validator.py
@@ -1,14 +1,21 @@
 import re
 import json
+from validators import (
+    cbu_validator,
+    cuit_validator,
+    great_than_validator,
+)
 
 DATA_TYPE_VALIDATOR = {
     "string": r"(.)+",
+    "xstring": r"\\s+$",
     "email": r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
     "cuit": r"(.)+",
     "ISO3166": r"^([A-Z]{0,2})$",
     "cbu": r"(.)+",
     "date": r"(.)+",
     "number": r"[-+]?[0-9]+(\.[0-9]+)?$",
+    "greater_than_10": great_than_validator
 }
 
 
@@ -70,8 +77,9 @@ class JSONEvaluator():
     def _valid_data(self, datatype, data):
         valid = False
         validator = DATA_TYPE_VALIDATOR.get(datatype)
+        assert validator is not None
         if callable(validator):
-            valid = validator(data)
+            valid = validator(data, datatype=datatype)
         else:
             pattern = re.compile(validator)
             data = str(data) if type(data) is not str else data
@@ -107,7 +115,14 @@ class JSONEvaluator():
                     except AssertionError as e:
                         self.found_errors.append(e.args[0])
                 else:
-                    assert self._valid_data(datatype, data), \
+                    try:
+                        valid = self._valid_data(datatype, data)
+                    except AssertionError:
+                        raise AssertionError(
+                            f"pattern {datatype} for {key} "
+                            "was not found in schema")
+
+                    assert valid, \
                         f"{key} is not well formatted"
             except AssertionError as e:
                 self.found_errors.append(e.args[0])
@@ -128,20 +143,34 @@ class JSONEvaluator():
         return DATA_TYPE_VALIDATOR
 
 
+
 if __name__ == "__main__":
     SCHEMA_STRUCTURE = {
-        "!version": "string",
+        "!version": "greater_than_10",
         "!email": "email",
         "!data": {
+            "!subrama": {
+                  "!numero": "number",
+                  "!otrasubrama": {
+                      "!id": "number"
+                  }
+                },
+
             "!Id": "number",
             "title": "string"  # optional
             },
         }
 
     ok_json = {
-        "version": "1.0.1a",
+        "version": "2",
         "email": "test@test.com",
         "data": {
+            "subrama": {
+                  "numero": 18,
+                  "otrasubrama": {
+                      "id": 555
+                  }
+                },
             "Id": 231,
             "title": "description"
         }
@@ -160,6 +189,6 @@ if __name__ == "__main__":
     e.evaluate(ok_json)
     print(e.errors)
     print(e.ok)
-    e.evaluate(bad_json)
-    print(e.errors)
-    print(e.ok)
+    # e.evaluate(bad_json)
+    # print(e.errors)
+    # print(e.ok)
